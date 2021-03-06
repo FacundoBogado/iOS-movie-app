6117f25391bfaf9b116b8a431da60890c20cb79d
Facundo Bogado <facundo.bogado@kindandcarta.com>
2021-03-06 10:38:53 -0300

    Initial Commit

---
+++ iOS movie app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS movie app
+//
+//  Created by Facundo Bogado on 06/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
